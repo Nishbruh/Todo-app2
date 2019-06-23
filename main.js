d1d62feaafadeca6e0b0c403a97ab9a24eb2d746
Nishbruh <nishchayaman@gmail.com>
2019-06-23 15:40:08 +0545

    minor changes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const userinput = document.querySelector('#userinput');
 const buttonadd = document.querySelector('#add')
 const userlist = document.querySelector('#users');
 const errortxt = document.querySelector('#errortxt');
+const style = document.createElement('style');
 window.addEventListener('load', () => {
     add_users();
     //delete_users();
@@ -13,10 +14,20 @@ const add_users = () => {
         clearafterenter();
     })
     userinput.addEventListener('click', clear);
+
 }
 const validate = () => {
     if (userinput.value === '') {
         errortxt.innerText = 'plz fill all the fields';
+        const style = document.createElement('style');
+        const head = document.querySelector('head');
+        style.innerHTML = `#errortxt{
+            color: red;
+            font-weight: none;
+            font-size: 12px;
+            background-color: white;
+        }`;
+        head.appendChild(style);
         userinput.classList.add('error');
         userinput.focus();
         return;
@@ -27,21 +38,32 @@ const validate = () => {
         };
         users = [...users, userid];
         generatelist(users);
+        minorclearance();
     }
 }
 const actions = (bg_color, message) => {
-    errortxt.style.color = 'white';
-    errortxt.style.backgroundColor = bg_color;
+    const style = document.createElement('style');
+    const head = document.querySelector('head');
+    style.innerHTML = `#errortxt{
+        color: white ;
+        background-color: ${bg_color};
+    }`;
+    head.appendChild(style);
     errortxt.innerText = message;
-    errortxt.style.width = 20;
 }
 
 const clear = () => {
-    errortxt.innerText = '';
+    const style = document.createElement('style');
+    errortxt.innerHTML = '';
     userinput.classList.remove('error');
+    console.log(style);
+    console.log(userinput);
 }
 const clearafterenter = () => {
     userinput.value = '';
+}
+const minorclearance = () => {
+    userinput.classList.remove('error');
 }
 
 
@@ -57,7 +79,6 @@ const generatelist = (users) => {
     const joined = list.join('');
     userlist.innerHTML = joined;
     actions('green', 'Details Added');
-    console.log(joined);
     return;
     /*document.addEventListener('click', (e) => {
 
